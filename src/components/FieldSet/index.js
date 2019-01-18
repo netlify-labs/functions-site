94592e34e245ebd9c9f76fab4861e407611ed56d
DavidWells <hello@davidwells.io>
2019-01-17 16:24:48 -0800

    speeling

--- src/components/FieldSet/index.js
+++ src/components/FieldSet/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-import styles from './FieldSet.css'
+import styles from './Fieldset.css'
 
 const propTypes = {
   /** Custom CSS Classes */
