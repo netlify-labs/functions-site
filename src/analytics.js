ae0edd52d889f571392062c5cb3b5536d0f10bb5
DavidWells <hello@davidwells.io>
2019-05-03 23:00:48 -0700

    add segment plugin

--- src/analytics.js
+++ src/analytics.js
@@ -1,10 +1,15 @@
 import Analytics from 'analytics'
-import gtagManager from 'analytics-plugin-google-tag-manager'
+import segmentPlugin from 'analytics-plugin-segment'
+import gtagManagerPlugin from 'analytics-plugin-google-tag-manager'
 
 const analytics = Analytics({
   plugins: [
-    gtagManager({
+    gtagManagerPlugin({
       containerId: 'GTM-NMKKF2M'
+    }),
+    segmentPlugin({
+      writeKey: 'f3W8BZ0iCGrk1STIsMZV7JXfMGB7aMiW',
+      disableAnonymousTraffic: true,
     }),
     {
       NAMESPACE: 'test-plugin',
